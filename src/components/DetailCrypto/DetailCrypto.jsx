28cddc7427bf4dc8a5993240f67796c590e6e530
Ivan Ruiperez Parra <ivan.ruiperez@gmail.com>
2020-10-31 19:01:37 +0100

    add Detail functions

--- src/components/DetailCrypto/DetailCrypto.jsx
+++ src/components/DetailCrypto/DetailCrypto.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import CryptoStore from "../../stores/crypto-store";
-import { loadCryptoCoin } from "../../actions/action-creators";
+import { loadCoinByID } from "../../actions/action-creators";
 
 function CryptoDetail() {
   const [cryptoCoin, setCryptCoin] = useState(null);
@@ -12,14 +12,43 @@ function CryptoDetail() {
   useEffect(() => {
     CryptoStore.addEventListener(handleChange);
     if (!cryptoCoin) {
-      loadCryptoCoin("bitcoin");
+      loadCoinByID("bitcoin");
     }
     return () => {
       CryptoStore.removeEventListener(handleChange);
     };
   }, [cryptoCoin]);
 
-  return <div>Pruebas</div>;
+  return (
+    <>
+      {cryptoCoin ? (
+        <section>
+          <figure>
+            <img src={cryptoCoin.data.image.small} alt="crypto__logo" />
+            <figcaption>{cryptoCoin.data.name}</figcaption>
+          </figure>
+          <div>
+            <span>
+              € {cryptoCoin.data.market_data.current_price.eur}(
+              {cryptoCoin.data.market_data.price_change_percentage_24h.toFixed(
+                2
+              )}
+              %)
+            </span>
+            <span>
+              $ {cryptoCoin.data.market_data.current_price.usd}(
+              {cryptoCoin.data.market_data.price_change_percentage_24h.toFixed(
+                2
+              )}
+              %)
+            </span>
+          </div>
+        </section>
+      ) : (
+        <div>Loading...</div>
+      )}
+    </>
+  );
 }
 
 export default CryptoDetail;
