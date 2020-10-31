e9581e579c9741044a8cd8043e4c9b8e0c56709d
Verónica Marull <vero@dieresys.com.ar>
2020-10-31 19:40:59 +0100

    add navbar

---
+++ src/components/NavCrypto/NavCrypto.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './NavCrypto.css';
+
+function NavCrypto() {
+    return (
+        <nav className="navbar__mobile">
+            <span><a href>Home</a></span>
+            <span><a href>About</a></span>
+            <span><a href>Log In</a></span>
+        </nav>
+    )
+}
+
+
+
+
+export default NavCrypto
