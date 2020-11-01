543dfa05e6758dbc1fa9936aa6d966fcec48d9e4
Jorge <jorgengarriga@gmail.com>
2020-11-01 15:41:00 +0100

    Add function return all coins global info

--- src/actions/action-creators.js
+++ src/actions/action-creators.js
@@ -3,15 +3,30 @@ import actionTypes from "./action-types";
 import CoinGecko from "coingecko-api";
 const coinGeckoClient = new CoinGecko();
 
-export async function loadCoinsMarkets() {
+export async function loadCoinsAll(show = 25, curPage = 0) {
+  //Return array with 25 coins global informatio
+  // including market info
+  const params = {
+    order: CoinGecko.ORDER.GECKO_DESC,
+    per_page: show,
+    page: curPage,
+  };
+
+  const coinAll = await coinGeckoClient.coins.all({ ...params });
+  console.log(coinAll);
+}
+
+export async function loadCoinsMarkets(show = 25, curPage = 0) {
   //Return an array with 25 coins market data (price, market cap, volume).
   const params = {
     order: CoinGecko.ORDER.GECKO_DESC,
-    per_page: 25,
+    per_page: show,
+    page: curPage,
     localization: false,
     vs_currency: "eur",
   };
   const coinMarkets = await coinGeckoClient.coins.markets({ ...params });
+  console.log(coinMarkets);
   dispatcher.dispatch({
     type: actionTypes.LOAD_CRYPTO_MARKETS_LIST,
     payload: coinMarkets.data,
