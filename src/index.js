b69cf9a5803551d59c885bfe169f37e97245beed
Jorge <jorgengarriga@gmail.com>
2020-11-01 14:34:46 +0100

    main navigation

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./index.css";
-import Main from "./components/Main/Main";
+import NavCrypto from "./components/NavCrypto/NavCrypto";
+import ListCrypto from "./components/ListCrypto/ListCryptoCurrency";
+import DetailCrypto from "./components/DetailCrypto/DetailCrypto";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Main />
+    <BrowserRouter>
+      <NavCrypto />
+      <Switch>
+        <Route path="/" exact component={ListCrypto} />
+        <Route path="/detail/:cryptoId" component={DetailCrypto} />
+      </Switch>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
