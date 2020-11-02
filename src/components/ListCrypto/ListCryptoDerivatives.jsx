cf6fc0ac0a7166c8677a39bbc5f43cfbfc0b753a
Verónica Marull <vero@dieresys.com.ar>
2020-11-02 10:23:12 +0100

    add changes

--- src/components/ListCrypto/ListCryptoDerivatives.jsx
+++ src/components/ListCrypto/ListCryptoDerivatives.jsx
@@ -2,8 +2,11 @@ import React, { useEffect, useState } from "react";
 import cryptoStore from "../../stores/crypto-store";
 import { loadDerivativesList } from "../../actions/action-creators";
 
+
+
 function ListCryptoDerivatives() {
     const [derivativesList, setDerivativesList] = useState(cryptoStore.getCryptoDerivatives());
+    
 
     function handleChange() {
         setDerivativesList(cryptoStore.getCryptoDerivatives());
@@ -19,64 +22,41 @@ function ListCryptoDerivatives() {
             cryptoStore.removeEventListener(handleChange);
         };
     }, [derivativesList]);
+
+    
     
     return (
         <>
             <div>
                 <div>
-                    <ul>Name:
-                        {derivativesList ? 
-                            ( derivativesList.data.map((data) => (
-                            <>
-                                <li key={data.id}><img alt="lala" src={data.image}/>
-                                    {data.name}
-                                </li>
-                            </>    
-                            ))
-                            ) : <li>Loading</li>}
-                    </ul>
-                </div>
-                <div>
-                    <ul>Perpetual Pairs:
+                    <ul className="derivativesList">Derivatives
                         {derivativesList ? 
                             ( derivativesList.data.map((data) => (
                             <>
                                 <li key={data.id}>
-                                    {data.number_of_perpetual_pairs}
+                                    <img alt="lala" src={data.image}/>
+                                    {`${data.name} 
+                                    Number of Perpetual Pairs: ${data.number_of_perpetual_pairs} 
+                                    Futures Pairs: ${data.number_of_futures_pairs} 
+                                    Website: ` } {<a href={data.url}>{data.name}</a>} 
                                 </li>
                             </>    
                             ))
                             ) : <li>Loading</li>}
                     </ul>
                 </div>
-                <div>
-                    <ul>Futures Pairs:
-                        {derivativesList ? 
-                            ( derivativesList.data.map((data) => (
-                            <>
-                                <li key={data.id}>
-                                    {data.number_of_futures_pairs}
-                                </li>
-                            </>    
-                            ))
-                            ) : <li>Loading</li>}
-                    </ul>
-                </div>
-                <div>
-                    <ul>Website
-                        {derivativesList ? 
-                            ( derivativesList.data.map((data) => (
-                            <>
-                                <li key={data.id}>
-                            <a href={data.url}>{data.name}</a>
-                                </li>
-                            </>    
-                            ))
-                            ) : <li>Loading</li>}
-                    </ul>
-                </div>
-                <div>
-                    <ul>Year Established:
+               
+                    
+                
+            </div>
+            
+            
+        </>
+    )
+}
+
+/*
+<ul>Year Established:
                         {derivativesList ? 
                             ( derivativesList.data.map((data) => {
                             
@@ -95,27 +75,9 @@ function ListCryptoDerivatives() {
                         })
                             ) : <li>Loading</li>}
                     </ul>
-                </div>
-            </div>
-        </>
-    )
-}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+*/
 
 
 
