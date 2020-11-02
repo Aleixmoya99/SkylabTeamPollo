210550156559932c1a6dc843a803ba784fb8c68e
Verónica Marull <vero@dieresys.com.ar>
2020-11-02 10:24:46 +0100

    descartar cambios

--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 import "./styles/normalize.css";
 import "./styles/index.css";
-import NavCrypto from "./components/NavCrypto/NavCrypto";
-import ListCrypto from "./components/ListCrypto/ListCryptoCurrency";
-import DetailCrypto from "./components/DetailCrypto/DetailCrypto";
+
+import ListCrypto from "./components/ListCrypto/ListCryptoDerivatives";
+
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <NavCrypto />
-      <Switch>
-        <Route path="/" exact component={ListCrypto} />
-        <Route path="/detail/:cryptoId" component={DetailCrypto} />
-      </Switch>
-    </BrowserRouter>
+    <ListCrypto />
   </React.StrictMode>,
   document.getElementById("root")
 );
