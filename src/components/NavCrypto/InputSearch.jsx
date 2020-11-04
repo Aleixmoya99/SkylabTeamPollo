f08fe5edcabc5b65094e7a259cbdc6879fa024cb
Jorge <jorgengarriga@gmail.com>
2020-11-04 16:22:28 +0100

    Fix classname at inputSearch

--- src/components/NavCrypto/InputSearch.jsx
+++ src/components/NavCrypto/InputSearch.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import "./InputSearch.css";
+import React from 'react';
+import './InputSearch.css';
 
 function InputSearch() {
-  return (
-    <>
-      <form class="search-desktop">
-        <input type="text" placeholder="Search.." name="search2" />
-        <button type="submit">
-          <i class="fa fa-search"></i>
-        </button>
-      </form>
-    </>
-  );
+	return (
+		<>
+			<form className="search-desktop">
+				<input type="text" placeholder="Search.." name="search2" />
+				<button type="submit">
+					<em className="fa fa-search"></em>
+				</button>
+			</form>
+		</>
+	);
 }
 
 export default InputSearch;
