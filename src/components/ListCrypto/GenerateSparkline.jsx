1eda0790bd5ec30075281d4a812dd54ab8c897f8
Jorge <jorgengarriga@gmail.com>
2020-11-05 11:38:20 +0100

    -

--- src/components/ListCrypto/GenerateSparkline.jsx
+++ src/components/ListCrypto/GenerateSparkline.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import { Sparklines, SparklinesLine, SparklinesSpots } from 'react-sparklines';
 
 function GenerateSparkline({ currentSparkline }) {
 	return (
 		<td key={currentSparkline.id}>
 			<Sparklines
 				data={currentSparkline.sparklines}
+				style={{ fill: 'none' }}
 				limit={167}
 				width={100}
 				height={50}
 				margin={1}
 			>
 				<SparklinesLine color="blue" />
+			</Sparklines>
+			<Sparklines data={currentSparkline.sparklines}>
+				<SparklinesLine />
+				<SparklinesSpots />
 			</Sparklines>
 		</td>
 	);
